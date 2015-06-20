9222fc1a152395fa41014b3e97d3ed9ed615cbde
Tom Bessems <tom.bessems@gmail.com>
2015-06-20 19:34:41 +0200

    Trying with original composer script

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -4,9 +4,11 @@
 php -v > /dev/null 2>&1
 PHP_IS_INSTALLED=$?
 
-if [[ $PHP_IS_INSTALLED -ne 0 ]]; then
-  echo "!!! PHP is not installed. Check the Vagrant file."; exit
-fi
+# Test if HHVM is installed
+hhvm --version > /dev/null 2>&1
+HHVM_IS_INSTALLED=$?
+
+[[ $HHVM_IS_INSTALLED -ne 0 && $PHP_IS_INSTALLED -ne 0 ]] && { printf "!!! PHP/HHVM is not installed.\n    Installing Composer aborted!\n"; exit 0; }
 
 # Test if Composer is installed
 composer -v > /dev/null 2>&1
@@ -15,25 +17,61 @@ COMPOSER_IS_INSTALLED=$?
 # Contains all arguments that are passed
 COMPOSER_ARG=($@)
 
+GITHUB_OAUTH=${COMPOSER_ARG[0]}
 COMPOSER_PACKAGES=${COMPOSER_ARG[@]:1}
 
 # True, if composer is not installed
 if [[ $COMPOSER_IS_INSTALLED -ne 0 ]]; then
     echo ">>> Installing Composer"
-    # Install Composer
-    curl -sS https://getcomposer.org/installer | php
-    sudo mv composer.phar /usr/local/bin/composer
+    if [[ $HHVM_IS_INSTALLED -eq 0 ]]; then
+        # Install Composer
+        sudo wget --quiet https://getcomposer.org/installer
+        hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 installer
+        sudo mv composer.phar /usr/local/bin/composer
+        sudo rm installer
+
+        # Add an alias that will allow us to use composer without timeout's
+        printf "\n# Add an alias for sudo\n%s\n# Use HHVM when using Composer\n%s" \
+        "alias sudo=\"sudo \"" \
+        "alias composer=\"hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 -v Eval.Jit=false /usr/local/bin/composer\"" \
+        >> "/home/vagrant/.profile"
+
+        # Resource .profile
+        # Doesn't seem to work do! The alias is only usefull from the moment you log in: vagrant ssh
+        . /home/vagrant/.profile
+    else
+        # Install Composer
+        curl -sS https://getcomposer.org/installer | php
+        sudo mv composer.phar /usr/local/bin/composer
+    fi
 else
     echo ">>> Updating Composer"
-    sudo composer self-update
+
+    if [[ $HHVM_IS_INSTALLED -eq 0 ]]; then
+        sudo hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 -v Eval.Jit=false /usr/local/bin/composer self-update
+    else
+        sudo composer self-update
+    fi
 fi
+
+if [[ $GITHUB_OAUTH -ne "" ]]; then
+    if [[ ! $COMPOSER_IS_INSTALLED -ne 0 ]]; then
+    else
+        composer config -g github-oauth.github.com $GITHUB_OAUTH
+    fi
+fi
+
 
 # Install Global Composer Packages if any are given
 if [[ ! -z $COMPOSER_PACKAGES ]]; then
 
     echo ">>> Installing Global Composer Packages:"
     echo "    " ${COMPOSER_PACKAGES[@]}
-    composer global require ${COMPOSER_PACKAGES[@]}
+    if [[ $HHVM_IS_INSTALLED -eq 0 ]]; then
+        hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 -v Eval.Jit=false /usr/local/bin/composer global require ${COMPOSER_PACKAGES[@]}
+    else
+        composer global require ${COMPOSER_PACKAGES[@]}
+    fi
 
     # Add Composer's Global Bin to ~/.profile path
     if [[ -f "/home/vagrant/.profile" ]]; then
